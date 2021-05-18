ce83987189bc6dc34ea20741e0227d840a3f6476
ba <baortusstu@gmail.com>
2021-05-18 08:36:57 -0600

    person repr

--- solution.py
+++ solution.py
@@ -1,6 +1,12 @@
 class Person:
-    def __init__(self):
-        pass
+    def __init__(self, id, name, age):
+        self.id = id
+        self.name = name
+        self.age = age
+
+    def __repr__(self):
+        strPerson = f"Person(id:{self.id}, name:{self.name}, age:{self.age})"
+        return strPerson
 
 class Developer:
     def __init__(self):
