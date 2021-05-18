cc643ca96f98240092345f08a855c537b4156ac4
ba <baortusstu@gmail.com>
2021-05-18 08:42:40 -0600

    developer repr

--- solution.py
+++ solution.py
@@ -5,12 +5,22 @@ class Person:
         self.age = age
 
     def __repr__(self):
-        strPerson = f"Person(id:{self.id}, name:{self.name}, age:{self.age})"
+        strPerson = f"Person(id: {self.id}, name: {self.name}, age: {self.age})"
         return strPerson
 
-class Developer:
-    def __init__(self):
-        pass
+class Developer(Person):
+    def __init__(self, id, name, age, salary, language):
+        super().__init__(id, name, age)
+        self.salary = salary
+        self.language = language
+
+    def __repr__(self):
+        strDev = (
+            f"Dev(id: {self.id}, name: {self.name}, "
+            +f"age: {self.age}, salary: {self.salary}, language: {self.language})"
+        )
+        return strDev
+
 
 class Roster:
     def __init__(self):
